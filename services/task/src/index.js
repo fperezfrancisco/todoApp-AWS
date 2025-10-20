8ab82aa288c627b2496770e6dc61e32d35dd6da7
Francisco Perez <fperezfrancisco4@gmail.com>
2025-10-19 19:04:41 -0700

    added task service setup

---
+++ services/task/src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4001;
+
+app.get("/", (req, res) => {
+  res.send("Hello from your Task Service!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Service running on port ${PORT}`);
+});
